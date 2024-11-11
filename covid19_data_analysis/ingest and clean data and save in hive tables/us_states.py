1bd9cf893e2abc3e7b9fb0a50f7a50c026eb9c9a
shaktimnepal <shaktimnepal1@gmail.com>
2024-11-10 17:23:46 -0800

    Create us_states.py

---
+++ covid19_data_analysis/ingest and clean data and save in hive tables/us_states.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession
+
+
+# Initialize Spark Session
+spark = SparkSession.builder.appName("AWS Project Covid-19 Analysis").enableHiveSupport().getOrCreate()
+
+
+# Read CSV file into DataFrame
+df = spark.read.csv("file:///home/takeo/data/aws_project/data_files/us_states.csv",header=True, inferSchema=True)
+
+
+# Drop duplicates
+df = df.dropDuplicates()
+
+
+# Handle null values with default replacements
+df = df.fillna({
+   "date": "unknown",
+   "state": "unknown",
+   "fips": 0,
+   "cases": 0,
+   "deaths": 0
+})
+
+
+
+
+# Write DataFrame to Hive as a Parquet table
+df.write.format("parquet").mode("overwrite").saveAsTable("aws_project.us_states_hive_table")
+
+
+# Query the table in Hive and display results
+spark.sql("select * from aws_project.us_states_hive_table limit 5").show()
